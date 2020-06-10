760e3292b61ab292493ffedd9aa06a733d9a59ae
Toan Huynh <kien.huynh@haileybury.vic.edu.au>
2020-06-10 16:09:11 +1000

    Updating Pyodide PyAngelo test sample Porting over more PyAngelo (brython) methods

--- PyAngelo/pyodide/pyangelo.py
+++ PyAngelo/pyodide/pyangelo.py
@@ -46,20 +46,20 @@ class PyAngelo():
         self.keys[KEY_V_DOWN] = False
         self.keys[KEY_V_FIRE] = False                               
 
-        #document.addEventListener("keydown", self._keydown)
-        #document.addEventListener("keyup", self._keyup)   
+        document.addEventListener("keydown", self._keydown)
+        document.addEventListener("keyup", self._keyup)   
         
         self.mouse_x = 0
         self.mouse_y = 0
-        #document.addEventListener("mousedown", self._mousedown)
-        #document.addEventListener("mouseup", self._mouseup)
-        #document.addEventListener("mousemove", self._mousemove)
+        document.addEventListener("mousedown", self._mousedown)
+        document.addEventListener("mouseup", self._mouseup)
+        document.addEventListener("mousemove", self._mousemove)
         
         self.touches = {}
         
-        #document.addEventListener("touchstart", self._touchstart)
-        #document.addEventListener("touchend", self._touchend)
-        #document.addEventListener("touchmove", self._touchmove)        
+        document.addEventListener("touchstart", self._touchstart)
+        document.addEventListener("touchend", self._touchend)
+        document.addEventListener("touchmove", self._touchmove)        
         
 
         self.soundPlayers = {}        
@@ -118,12 +118,6 @@ class PyAngelo():
         
         window.console.log("Attempting to load file:" + file);
         
-        '''
-        canvas = iodide.output.element('canvas')
-        canvas.setAttribute('width', 450)
-        canvas.setAttribute('height', 300)        
-        '''
-        
         img = document.createElement("IMG");
         img.crossOrigin = "Anonymous"
         img.src = file
@@ -131,11 +125,6 @@ class PyAngelo():
         jmssImg = PyAngeloImage(img, sprite)       
         self.resources[file] = jmssImg        
         
-
-        #img = html.IMG()
-        #img.crossOrigin = "Anonymous"
-        #img.src = file
-
         img.addEventListener('load', self.imageLoaded)
         #img.bind('error', self.resourceError)
         #img.bind('abort', self.resourceAbort)
@@ -174,11 +163,200 @@ class PyAngelo():
             self.ctx.restore()
         else:
             self.ctx.drawImage(image.img, x, self._convY(y + height), width, height)
-            
 
-
-        self.ctx.restore()          
+        self.ctx.restore()         
         
+    def drawLine(self, x1, y1, x2, y2, r = 1.0, g = 1.0, b = 1.0, a = 1.0, width = 1):
+        r = min(r, 1.0)
+        g = min(g, 1.0)
+        b = min(b, 1.0)
+        a = min(a, 1.0)
+
+        self.ctx.beginPath()
+        self.ctx.lineWidth = width
+        self.ctx.strokeStyle = "rgba(" + str(int(r * 255.0)) + "," + str(int(g * 255.0)) + "," + str(int(b * 255.0)) + "," + str(a) + ")"
+        self.ctx.moveTo(x1, self._convY(y1))
+        self.ctx.lineTo(x2, self._convY(y2))
+        self.ctx.stroke()
+        
+    def _keydown(self, ev):
+       
+        self.keys[ev.which] = True
+               
+        # pressing escape when the program has halted
+        if ev.which == KEY_ESC and self.state == self.STATE_HALT:
+            self.stop()
+            
+        # TODO: support stopping during INPUT state
+            
+        if self.state == self.STATE_INPUT:
+            if ev.which != KEY_ENTER and (ev.which < 32 or ev.which > KEY_A + 26):
+                return
+            array[len(array) - 1 - self.input_buffer_index] = ev.which
+            if ev.which == KEY_ENTER:
+                self.input_concluded = True
+            else:
+                returned_string = ""
+                n = self.input_buffer_index
+                while n >= 0:
+                
+                    returned_string = chr(array[len(array) - 1 - n]) + returned_string
+                    n -= 1
+                
+                self.input_buffer_index += 1
+                self.drawText(returned_string + "_", 0, 0)
+
+    def isKeyPressed(self, key):
+        return self.keys[key]             
+        
+
+    def _keyup(self, ev):
+        self.keys[ev.which] = False  
+
+
+    def _updateTouchedKeys(self):
+        self.keys[KEY_V_LEFT] = False
+        self.keys[KEY_V_RIGHT] = False
+        self.keys[KEY_V_UP] = False
+        self.keys[KEY_V_DOWN] = False
+        self.keys[KEY_V_FIRE] = False      
+        
+        for touch in self.touches.values():
+            x = touch[0]
+            y = touch[1]
+            
+            if x < -self.width * 0.33 * 0.67 and y < self.height * 0.6 and y > self.height * 0.4:
+                self.keys[KEY_V_LEFT] = True
+            if x < 0 and x > -self.width * 0.33 * 0.33 and y < self.height * 0.6 and y > self.height * 0.4:
+                self.keys[KEY_V_RIGHT] = True
+
+            if y < self.height * 0.4 and y > 0 and x < -self.width * 0.33 * 0.33 and x > -self.width * 0.33 * 0.67:
+                self.keys[KEY_V_DOWN] = True
+            if y < self.height and y > self.height * 0.6 and x < -self.width * 0.33 * 0.33 and x > -self.width * 0.33 * 0.67:
+                self.keys[KEY_V_UP] = True
+
+            if x > self.width and y < self.height and y > 0:
+                self.keys[KEY_V_FIRE] = True            
+
+    def _touchstart(self, ev):
+        #ev.preventDefault()
+        for touch in ev.changedTouches:
+            self.mouse_x = touch.clientX             
+            self.mouse_y = touch.clientY
+            
+            boundingRect = self.canvas.getBoundingClientRect()    
+            
+            x = int(self.mouse_x - boundingRect.left)
+            y = int(self.height - (self.mouse_y - boundingRect.top))
+            
+            self.touches[touch.identifier] = [x, y]
+            
+        self._updateTouchedKeys()
+                        
+        return False
+
+    def _touchend(self, ev):   
+        #ev.preventDefault()
+
+        self.mouse_x = -1
+        self.mouse_y = -1
+        
+        for touch in ev.changedTouches:
+            del self.touches[touch.identifier]
+        
+        
+        self._updateTouchedKeys()
+
+        return False
+        
+    def _touchmove(self, ev):   
+        #ev.preventDefault()
+        for touch in ev.changedTouches:
+            self.mouse_x = touch.clientX             
+            self.mouse_y = touch.clientY
+            
+            boundingRect = self.canvas.getBoundingClientRect()    
+            
+            x = int(self.mouse_x - boundingRect.left)
+            y = int(self.height - (self.mouse_y - boundingRect.top))
+            
+            self.touches[touch.identifier] = [x, y]
+            
+        self._updateTouchedKeys()            
+                        
+        return False
+        
+    def _mousemove(self, ev):
+        self.mouse_x = ev.clientX             
+        self.mouse_y = ev.clientY            
+        
+    def _mousedown(self, ev):
+        self.mouse_x = ev.clientX             
+        self.mouse_y = ev.clientY
+        
+        boundingRect = self.canvas.getBoundingClientRect()    
+        
+        x = int(self.mouse_x - boundingRect.left)
+        y = int(self.height - (self.mouse_y - boundingRect.top))
+        
+        self.keys[KEY_V_LEFT] = False
+        self.keys[KEY_V_RIGHT] = False
+        self.keys[KEY_V_UP] = False
+        self.keys[KEY_V_DOWN] = False
+        self.keys[KEY_V_FIRE] = False
+        
+        if x < -self.width * 0.33 * 0.67 and y < self.height * 0.6 and y > self.height * 0.4:
+            self.keys[KEY_V_LEFT] = True
+        if x < 0 and x > -self.width * 0.33 * 0.33 and y < self.height * 0.6 and y > self.height * 0.4:
+            self.keys[KEY_V_RIGHT] = True
+
+        if y < self.height * 0.4 and y > 0 and x < -self.width * 0.33 * 0.33 and x > -self.width * 0.33 * 0.67:
+            self.keys[KEY_V_DOWN] = True
+        if y < self.height and y > self.height * 0.6 and x < -self.width * 0.33 * 0.33 and x > -self.width * 0.33 * 0.67:
+            self.keys[KEY_V_UP] = True
+
+        if x > self.width and y < self.height and y > 0:
+            self.keys[KEY_V_FIRE] = True         
+        
+    def _mouseup(self, ev):
+        self.mouse_x = -1
+        self.mouse_y = -1
+        
+        self.keys[KEY_V_LEFT] = False
+        self.keys[KEY_V_RIGHT] = False
+        self.keys[KEY_V_UP] = False
+        self.keys[KEY_V_DOWN] = False
+        self.keys[KEY_V_FIRE] = False     
+    
+    def getMousePos(self):
+        boundingRect = self.canvas.getBoundingClientRect()    
+        
+        return Point(int(self.mouse_x - boundingRect.left), int(self.height - (self.mouse_y - boundingRect.top)))
+        
+
+
+    def drawText(self, text, x, y, fontName = "Arial", fontSize = 10, r = 1.0, g = 1.0, b = 1.0, a = 1.0, anchorX = "left", anchorY ="bottom"):
+        self.ctx.fillStyle = "rgba(" + str(int(r * 255.0)) + "," + str(int(g * 255.0)) + "," + str(int(b * 255.0)) + "," + str(a) + ")"
+        self.ctx.font = str(fontSize) + "pt " + fontName
+        self.ctx.textBaseline = "bottom"
+        self.ctx.fillText(text, x, self.height - y)        
+        
+    def drawRect(self, x, y, w, h, r = 1.0, g = 1.0, b = 1.0, a = 1.0):
+        r = min(r, 1.0)
+        g = min(g, 1.0)
+        b = min(b, 1.0)
+        a = min(a, 1.0)
+
+        ctx = self.ctx
+        ctx.fillStyle = "rgba(" + str(int(r * 255.0)) + "," + str(int(g * 255.0)) + "," + str(int(b * 255.0)) + "," + str(a) + ")"        
+        ctx.beginPath();
+        ctx.moveTo(x, self._convY(y))
+        ctx.lineTo(x + w, self._convY(y))
+        ctx.lineTo(x + w, self._convY(y + h))
+        ctx.lineTo(x, self._convY(y + h))
+        ctx.closePath()
+        ctx.fill()                  
+
     def loop(self, func):
         self.main_loop = func
         
