1ea77b53212bae88192311b1ab5fb67d06c9ade5
Toan Huynh <kien.huynh@haileybury.vic.edu.au>
2020-11-11 10:18:25 +1100

    Updating sample

--- angelo/projects/sequence.py
+++ angelo/projects/sequence.py
@@ -16,15 +16,14 @@ while x > -110:
     graphics.clear(0, 0, 0)
     graphics.drawText("Hello!", x, 100, fontSize = 30)
     x = x - 2
-    
+
 while graphics.isKeyPressed(KEY_A) == False:
     graphics.clear(0, 0, 0)
     graphics.drawText("Press A to start", 110, 200, fontSize = 30, r = 1, g = 0, b = 0)
     
-    
 r = 0
-x = 0
-y = 0
+x = 100
+y = 100
 direction = 0.01
 while True:
     graphics.clear(r, 0, 0)
@@ -42,5 +41,12 @@ while True:
         y = y + 1
     if graphics.isKeyPressed(KEY_S):
         y = y - 1
+    if x < 0:
+        graphics.drawText("You can't touch the edge!",0, 300, fontSize = 30)
+        graphics.sleep(1000)
+        x = 0
+    if graphics.isKeyPressed(KEY_Q):
+        break
     
-    
+graphics.clear(0, 1, 0)
+graphics.drawText("Game Over!!!", 0, 0, fontSize = 30)
