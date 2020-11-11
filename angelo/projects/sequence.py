c8214a7118b1046e2a116e54ee7ae124075aa866
Toan Huynh <kien.huynh@haileybury.vic.edu.au>
2020-11-11 11:48:49 +1100

    updating sample

--- angelo/projects/sequence.py
+++ angelo/projects/sequence.py
@@ -1,6 +1,7 @@
 graphics.clear(0, 0, 0)
 graphics.drawText("👇Look below", 0, 130, fontSize = 30)
 
+# pause
 graphics.sleep(1000)
 
 # moving text in
@@ -25,17 +26,15 @@ while graphics.isKeyPressed(KEY_A) == False:
     graphics.drawText("Press A to start", 110, 200, fontSize = 30, r = 1, g = 0, b = 0)
 
 # start our game!
-r = 0
+# level 1
 x = 100
 y = 100
-direction = 0.01
+
 while True:
-    graphics.clear(r, 0, 0)
-    r = r + direction
-    if r >= 1.0:
-        direction = -0.01
-    elif r <= 0.0:
-        direction = 0.01
+    graphics.clear(1, 0, 0)
+    graphics.drawText("👈 Don't go here", 0, 350, fontSize = 20)
+    graphics.drawText("Go here 👉", 360, 350, fontSize = 20)
+    graphics.drawText("Level 1", 200, 0, fontSize = 15)
     graphics.drawText("👶", x, y, fontSize = 30)
     if graphics.isKeyPressed(KEY_A):
         x = x - 1
@@ -50,12 +49,38 @@ while True:
         graphics.drawText("You can't touch the edge!",0, 300, fontSize = 30)
         graphics.sleep(1000)
         x = 0
-    # pressing Q to quit
-    if graphics.isKeyPressed(KEY_Q):
+    if x > 450:
+        # break out of the infinite loop to go to next level
+        break
+
+# level 2
+x = 50
+y = 100
+
+while True:
+    graphics.clear(0, 1, 0)
+    graphics.drawText("Go here to win! 👉", 275, 350, fontSize = 20)
+    graphics.drawText("Level 2", 200, 0, fontSize = 15)
+    graphics.drawText("👶", x, y, fontSize = 30)
+    if graphics.isKeyPressed(KEY_A):
+        x = x - 1
+    if graphics.isKeyPressed(KEY_D):
+        x = x + 1
+    if graphics.isKeyPressed(KEY_W):
+        y = y + 1
+    if graphics.isKeyPressed(KEY_S):
+        y = y - 1
+    if x < 0:
+        # if hits edge, display some text for 1 sec pause
+        graphics.drawText("You can't touch the edge!",0, 300, fontSize = 30)
+        graphics.sleep(1000)
+        x = 0
+    if x > 450:
+        # break out of the infinite loop to go to end!
         break
     
-# end game screen (or new level etc.)
-graphics.clear(0, 1, 0)
-graphics.drawText("Game Over!!!", 0, 0, fontSize = 30)
+# win game screen (or new level etc.)
+graphics.clear(0, 0, 1)
+graphics.drawText("You won!!!", 150, 200, fontSize = 30)
     
     
