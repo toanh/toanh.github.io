4aa9c6c5b8916da6d595a80636b1797046ee4d92
Toan Huynh <kien.huynh@haileybury.vic.edu.au>
2021-03-18 16:08:13 +1100

    Adding fractal demo

---
+++ angelo/projects/fractal01.py
@@ -0,0 +1,25 @@
+def midpoint(p1, p2):
+    return((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
+
+triangles = [[(50, 375), (450, 375), (250, 29)]]
+
+colour = 1.0
+depth = 0
+
+graphics.clear()
+
+while depth < 6:
+    next_level = []
+    
+    for tri in triangles:
+        new_tri = []
+        for i in range(3):
+            first = midpoint(tri[i-1], tri[i])
+            second = midpoint(tri[i], tri[(i+1) % 3])
+            graphics.drawLine(first[0], first[1], second[0], second[1], r = 1 - colour, g = colour, b = 1 - colour)
+            new_tri += [[first, tri[i], second]]
+        next_level += new_tri
+    triangles = next_level
+    colour -= 0.1
+    depth = depth + 1
+    graphics.sleep(100)
